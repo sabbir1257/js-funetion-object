4287595cf950ea6dca4c8af485c8dcb1eb7550be
sabbir1257 <masabbir1257@gmail.com>
2023-02-04 15:24:10 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,10 @@
+//function declaration 
+function startFsn(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+
+
+// call the function
+startFsn();
